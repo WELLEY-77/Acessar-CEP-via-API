e293c338331f6660c7fc65ef50dd458ce1654273
Welley <monteirowelley7@gmail.com>
2023-01-05 13:45:07 -0300

    criação da função para formata a mascara do CEP, mostra-la

--- acessar_cep.py
+++ acessar_cep.py
@@ -11,3 +11,16 @@ class BuscarEndereco():
             return True
         else:
             return False
+    
+    def format_cep(self):
+        return f'{self.cep[:5]}-{self.cep[5:]}'
+    
+
+    def __str__(self):
+        return self.format_cep()
+
+
+
+object_cep = BuscarEndereco('62540970')
+
+print(object_cep)
