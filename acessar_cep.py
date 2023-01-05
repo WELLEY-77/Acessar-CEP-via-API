f8f4d596c24a7fcdaa2113c110c6ec4437b68b44
Welley <monteirowelley7@gmail.com>
2023-01-05 12:36:40 -0300

    modulo valida cep criado, e transformando cep em str

--- acessar_cep.py
+++ acessar_cep.py
@@ -1,3 +1,13 @@
 class BuscarEndereco():
     def __init__(self, cep):
-        self.cep = cep
+        cep = str(cep)
+        if self.validar_cep(cep):
+            self.cep = cep
+        else:
+            raise ValueError('CEP invalido!!')
+    
+    def validar_cep(self, cep):
+        if len(cep) == 8:
+            return True
+        else:
+            return False
